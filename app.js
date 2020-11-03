0752715b941c5a3ed76d3abe6157c33276f23909
carlos <ecommerce@avilasi.com>
2020-11-03 17:50:10 -0300

    revisión

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 const argv = require('./config/yargs').argv;
+const colors = require('colors');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
 switch( comando ){
@@ -18,4 +20,3 @@ switch( comando ){
 
 // let parametro = argv[2]
 // let base = parametro.split('=')[1];
-
